c58f32818046ebef57057a3853ca34d1f96ac361
League Student <league@iMac-21.attlocal.net>
2019-11-03 14:26:48 -0800

    Summary (required)
    Description

--- src/_01_intro_to_static/Raceyboi.java
+++ src/_01_intro_to_static/Raceyboi.java
@@ -13,12 +13,19 @@ package _01_intro_to_static;
 	    Raceyboi (String name, int speed){
 	        this.name = name;
 	        this.speed = speed;
+	        nextBibNumber++;
+	        bibNumber = nextBibNumber;
 	    }
 
 	    public static void main(String[] args) {
 	        //create two athletes
-	    
+	    Raceyboi racer1 = new Raceyboi("Jackson" , 5);
+	    Raceyboi racer2 = new Raceyboi("Jackson's brother in law that he didn't consent to have but he did anyway" , 8);
 	        //print their names, bibNumbers, and the location of their race. 
+	   racer1.name="Jackson";
+	  racer2.name="Jackson's brother in law that he didn't consent to have but he did anyway";
+System.out.println("Jackson");
+System.out.println("Jackson's brother in law that he didn't consent to have but he did anyway");
+
 	    }
 	}
-
